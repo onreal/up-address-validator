e7b724496d967d460c822b14bd49796b78bcd12a
Margarit Koka <panagiotis.kokas@gmail.com>
2022-08-23 17:00:07 +0300

    fix: autoloader watch inc path

--- Autoloader.php
+++ Autoloader.php
@@ -3,14 +3,14 @@ class Autoloader
 {
     public static function register()
     {
-        spl_autoload_register(function ($class) {
+        spl_autoload_register( function ( $class ) {
             $prefix = 'Upio\\UpAddressValidator\\';
-            $len = strlen($prefix);
+            $len = strlen( $prefix );
             // make sure that we include only our prefixed namespace
-            if (strncmp($prefix, $class, $len) !== 0) return;
-            $className = substr($class, $len);
-            $file = UP_ADDRESS_VALIDATION_PATH . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
-            if (file_exists($file)) { require $file;}
+            if ( strncmp( $prefix, $class, $len ) !== 0 ) return;
+            $className = substr( $class, $len );
+            $file = UP_AV_INC_PATH . str_replace( '\\', DIRECTORY_SEPARATOR, $className ) . '.php';
+            if ( file_exists( $file ) ) { require $file; }
         });
     }
 }
